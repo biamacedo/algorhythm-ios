ff6d7489f0d4808b5845dc8b8a986500d92b45ac
bianet13 <bia.macedo13@gmail.com>
2015-08-09 15:25:14 -0700

    Finished Build a Playlist Browser with Swift

---
+++ Algorhythm/PlaylistMasterVIewController.swift
@@ -0,0 +1,62 @@
+//
+//  PlaylistMasterViewController.swift
+//  Algorhythm
+//
+//  Created by Beatriz Macedo on 8/8/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+    
+    var playlistArray: [UIImageView] = []
+    
+    @IBOutlet weak var playlistImageView0: UIImageView!
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playlistArray += [  playlistImageView0,
+                            playlistImageView1,
+                            playlistImageView2,
+                            playlistImageView3,
+                            playlistImageView4,
+                            playlistImageView5]
+        
+        for i in 0..<playlistArray.count {
+            let playlist = Playlist(index: i)
+            let playlistImageView = playlistArray[i]
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showPlaylistDetailSegue" {
+            let playlistImageView = sender!.view as! UIImageView
+            if let i = find(playlistArray, playlistImageView){
+                let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+                playlistDetailController.playlist = Playlist(index: i)
+            }
+            
+        }
+        
+    }
+
+    @IBAction func showPlaylistDetail(sender: AnyObject) {
+        performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
+    }
+}
+
